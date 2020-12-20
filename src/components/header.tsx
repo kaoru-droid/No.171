e02ea265c0b880789242022af6d76cb6dd6b02d5
kaoru-droid <67271607+kaoru-droid@users.noreply.github.com>
2020-12-20 14:24:50 +0900

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,13 +5,13 @@ import { useRouter } from 'next/router'
 import styles from '../styles/header.module.css'
 
 const navItems: { label: string; page?: string; link?: string }[] = [
-  { label: 'Home', page: '/' },
-  { label: 'Blog', page: '/blog' },
-  { label: 'Contact', page: '/contact' },
-  { label: 'Source Code', link: 'https://github.com/ijjk/notion-blog' },
+  //{ label: 'Home', page: '/' },
+  //{ label: 'Blog', page: '/blog' },
+  //{ label: 'Contact', page: '/contact' }.
+  //{ label: 'Source Code', link: 'https://github.com/ijjk/notion-blog' },
 ]
 
-const ogImageUrl = 'https://notion-blog.now.sh/og-image.png'
+/*const ogImageUrl = 'https://notion-blog.now.sh/og-image.png'*/
 
 export default ({ titlePre = '' }) => {
   const { pathname } = useRouter()
@@ -19,16 +19,8 @@ export default ({ titlePre = '' }) => {
   return (
     <header className={styles.header}>
       <Head>
-        <title>{titlePre ? `${titlePre} |` : ''} My Notion Blog</title>
-        <meta
-          name="description"
-          content="An example Next.js site using Notion for the blog"
-        />
-        <meta name="og:title" content="My Notion Blog" />
-        <meta property="og:image" content={ogImageUrl} />
-        <meta name="twitter:site" content="@_ijjk" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content={ogImageUrl} />
+        <h1>NO.171</h1>
+        <title>{titlePre ? `${titlePre} |` : ''} No.171</title>
       </Head>
       <ul>
         {navItems.map(({ label, page, link }) => (
