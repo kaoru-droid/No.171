e643f5103ebe59ad46530ce0974c3c864813938a
kaoru-droid <67271607+kaoru-droid@users.noreply.github.com>
2021-03-03 16:15:13 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -92,7 +92,7 @@ export default ({ posts = [], preview }) => {
                 <div className="authors">🙍‍♂️{post.Authors.join(' ')}</div>
               )}
               {post.Date && (
-                <div className="posted"> 📅{getDateStr(post.Date)}</div>
+                <div className="posted"> //{getDateStr(post.Date)}</div>
               )}
               <p>
                 {(!post.preview || post.preview.length === 0) && ''}
