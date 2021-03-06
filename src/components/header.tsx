63bee0f678767403d68a3465ad546860716868ca
kaoru-droid <67271607+kaoru-droid@users.noreply.github.com>
2021-03-06 21:41:12 +0900

    Update header.tsx
    heder 新しいやつ

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,11 +4,23 @@ import ExtLink from './ext-link'
 import { useRouter } from 'next/router'
 import styles from '../styles/header.module.css'
 
-const navItems: { label: string; page?: string; link?: string }[] = [
-  { label: 'Home', page: '/' },
-  { label: 'Blog', page: '/blog' },
+const navHome: { label: string; page?: string; link?: string }[] = [
+  { label: 'No.171', page: '/' },
+  // { label: 'Blog', page: '/blog' },
   //{ label: 'Contact', page: '/contact' }.
-  { label: 'Travel', link: '/blog/travel' },
+  // { label: 'Travel', link: '/blog/2021-03-05' },
+]
+const navArticle: { label: string; page?: string; link?: string }[] = [
+  // { label: 'Home', page: '/' },
+  { label: 'All Articles', page: '/blog' },
+  //{ label: 'Contact', page: '/contact' }.
+  // { label: 'Travel', link: '/blog/2021-03-05' },
+]
+const navTravel: { label: string; page?: string; link?: string }[] = [
+  // { label: 'Home', page: '/' },
+  // { label: 'Blog', page: '/blog' },
+  //{ label: 'Contact', page: '/contact' }.
+  { label: 'Travel', link: '/blog/2021-03-05' },
 ]
 
 /*const ogImageUrl = 'https://notion-blog.now.sh/og-image.png'*/
@@ -22,10 +34,38 @@ export default ({ titlePre = '' }) => {
         <title>{titlePre ? `${titlePre} |` : ''} No.171</title>
       </Head>
 
-      <h1>No.171</h1>
-
       <ul>
-        {navItems.map(({ label, page, link }) => (
+        {navHome.map(({ label, page, link }) => (
+          <li key={label}>
+            {page ? (
+              <Link href={page}>
+                <a className={pathname === page ? 'active' : undefined}>
+                  {label}
+                </a>
+              </Link>
+            ) : (
+              <ExtLink href={link}>{label}</ExtLink>
+            )}
+          </li>
+        ))}
+      </ul>
+      <ul>
+        {navArticle.map(({ label, page, link }) => (
+          <li key={label}>
+            {page ? (
+              <Link href={page}>
+                <a className={pathname === page ? 'active' : undefined}>
+                  {label}
+                </a>
+              </Link>
+            ) : (
+              <ExtLink href={link}>{label}</ExtLink>
+            )}
+          </li>
+        ))}
+      </ul>
+      <ul>
+        {navTravel.map(({ label, page, link }) => (
           <li key={label}>
             {page ? (
               <Link href={page}>
